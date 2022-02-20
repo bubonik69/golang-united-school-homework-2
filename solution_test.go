2193f560ed734c424fe80126996a434a0c2bda53
Artem <ruteboy69@gmail.com>
2022-02-21 02:29:54 +0300

    6

--- solution_test.go
+++ solution_test.go
@@ -11,37 +11,6 @@ const (
 
 
 
-func TestCalc (t *testing.T){
-	testCases :=[]struct{
-		sideLen float64
-		sidesNum sideNumType
-		square	float64
-	}{
-		{
-			sideLen: 10,
-			sidesNum: SidesCircle,
-			square:  314.1592653589793,
-		},
-		{
-			sideLen: 10,
-			sidesNum: SidesSquare,
-			square : 100,
-		},
-		{
-			sideLen: 10,
-			sidesNum: SidesTriangle,
-			square : 43.30127018922193,
-		},
-	}
-	for _,tc:=range testCases{
-		//tc:=tc
-		actual:=CalcSquare(tc.sideLen, tc.sidesNum)
-		if tc.square-actual>0.01{
-			t.Errorf("test finished with error, result of test: ")
-		}
-	}
-	t.Log("OK!")
-}
 
 func TestCalcSquare(t *testing.T) {
 	var expects float64 = 100
@@ -56,6 +25,7 @@ func diff(t *testing.T, expects, actual float64) {
 	}
 
 	t.Log("OK!")
+
 }
 
 
